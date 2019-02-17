46cb79cd244db792db4316474522bc9aca919357
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2019-02-17 22:42:37 +0200

    change state x/o

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-function  Square(props){
+function Square(props) {
 
-        return (
-            <button
-                className="square"
-                onClick={props.onClick}>
-                {props.value}
-            </button>
-        );
+    return (
+        <button
+            className="square"
+            onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
@@ -19,13 +19,17 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        });
     }
 
     renderSquare(i) {
