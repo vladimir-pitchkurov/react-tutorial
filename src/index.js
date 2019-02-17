2e8a5c6e69194cf769ea9d7c02a87f2b42f10de8
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2019-02-17 22:37:42 +0200

    onClick optimization

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ function  Square(props){
         return (
             <button
                 className="square"
-                onClick={() => {
-                    props.onClick();
-                }}>
+                onClick={props.onClick}>
                 {props.value}
             </button>
         );
